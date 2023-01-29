2111acde6d64aaa38d6472442661740c67792b09
0214jj <jeeyoun0214@gmail.com>
2023-01-30 01:03:36 +0900

    [add] bgPresetPage components

---
+++ wrdr_front/pages/Setting/SetBackgroundPage.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import PresetMenu from '../../components/Preset/PresetMenu';
+import CircleBG from '../../components/Preset/CircleBG';
+
+const SetBackgroundPage = () => {
+  return (
+    <View style={styles.container}>
+      <PresetMenu />
+      <Text style={styles.text}>우리의 이야기는 어디서 시작하나요?</Text>
+      <CircleBG style={styles.circle1} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    alignContent: 'center',
+    backgroundColor: '#151B3B',
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 35,
+    color: '#FFFFFF',
+    fontFamily: 'Jalnan',
+    paddingTop: 30,
+    paddingBottom: 30,
+  },
+  circle1: {
+    alignContent: 'center',
+  },
+});
+
+export default SetBackgroundPage;
