fe4b0329cd1d300d223c3a7552a882af14fba667
0214jj <jeeyoun0214@gmail.com>
2023-04-06 14:45:52 +0900

    [add] image 크기 조정
    * 선택한 표지 이미지가 티켓 사이즈에 맞도록 조정함.

--- wrdr_front/components/AfterStory/TicketImageBox.js
+++ wrdr_front/components/AfterStory/TicketImageBox.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Styled from 'styled-components/native';
-import { Keyboard } from 'react-native';
+import { Keyboard, StyleSheet, Image } from 'react-native';
+import ImageResizeMode from 'react-native/Libraries/Image/ImageResizeMode';
 
 const Container = Styled.View`
   width: 50%;
@@ -36,7 +37,8 @@ const TicketViewImage = Styled.ImageBackground`
   opacity: 0.6;
   margin-left: 10;
   margin-right: 10;
-  paddingbottom: 0;›
+  paddingbottom: 0;
+  display: ${props => (props.isVisible ? 'none' : 'flex')};
 `;
 
 const Text = Styled.Text`
@@ -51,8 +53,8 @@ const TicketTitleView = Styled.View`
     height: 25%;
     background-color: #ffffff;
     border-radius: 20;
+    
 `;
-
 const TitleInput = Styled.TextInput`
   color: #000000;
   font-size: 30;
@@ -65,11 +67,21 @@ const TitleInput = Styled.TextInput`
 `;
 
 const TicketImageBox = ({ ticketInfo, pageType, imageInfo }) => {
-  //console.log(imageInfo.imageSrc);
+  console.log(ticketInfo.storyTitle);
+  const [hasKeyboard, setHasKeyboard] = useState(false);
+  const shownKeyboard = () => {
+    //키보드가 보이면 ticketviewimage가 안보이게(사라지게) 키보드 사라지면 다시 보이게
+    if (Keyboard.isVisible) {
+      setHasKeyboard(true);
+    } else {
+      setHasKeyboard(false);
+    }
+  };
+
   return (
     <>
       <Container>
-        {!Keyboard.isVisible() && !ticketInfo.isActive.ticketImage ? (
+        {!ticketInfo.isActive.ticketImage ? (
           <TicketViewText>
             {'\n'}
             {'\n'}
@@ -79,17 +91,46 @@ const TicketImageBox = ({ ticketInfo, pageType, imageInfo }) => {
             {'\n'}그림을{'\n'}
             {'\n'}선택해주세요!
           </TicketViewText>
-        ) : !Keyboard.isVisible() && ticketInfo.isActive.ticketImage ? (
-          <TicketViewImage source={imageInfo.imageSrc} />
-        ) : Keyboard.isVisible() ? (
-          <TicketViewImage style={{ display: 'none' }} />
+        ) : ticketInfo.isActive.ticketImage ? (
+          <Image style={styles.TicketViewImage} source={imageInfo.imageSrc} isVisible={hasKeyboard} />
         ) : (
           ''
         )}
-        <TicketTitleView>{pageType === 'storyTitle' ? <TitleInput multiline placeholder="이곳을 클릭해 제목을 입력해주세요!" maxLength={20} /> : <TitleInput editable={false} />}</TicketTitleView>
+        <TicketTitleView>
+          {pageType === 'storyTitle' ? (
+            <TitleInput
+              onClick={shownKeyboard}
+              onChange={event => {
+                ticketInfo.storyTitle = event.nativeEvent.text; //setticketinfo
+              }}
+              multiline
+              placeholder="이곳을 클릭해 제목을 입력해주세요!"
+              maxLength={20}
+            />
+          ) : (
+            <TitleInput editable={false} />
+          )}
+        </TicketTitleView>
       </Container>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  TicketViewImage: {
+    width: '57%',
+    height: '75%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000',
+    borderRadius: 20,
+    opacity: 0.6,
+    marginLeft: 10,
+    marginRight: 10,
+    paddingBottom: 0,
+    resizeMode: 'cover',
+    // display: ${props => (props.isVisible ? 'none' : 'flex')},
+  },
+});
 
 export default TicketImageBox;
